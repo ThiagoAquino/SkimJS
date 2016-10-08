4e02a7e59961bd57758473dc6ea3aa5a3148c6ec
ThiagoAquino <thiagoaquiino@gmail.com>
2016-10-08 17:25:13 -0300

    Funcionando o IF, IF/ELSE, FOR, BREAK

--- Main.hs
+++ Main.hs
@@ -29,6 +29,104 @@ evalStmt env (VarDeclStmt []) = return Nil
 evalStmt env (VarDeclStmt (decl:ds)) =
     varDecl env decl >> evalStmt env (VarDeclStmt ds)
 evalStmt env (ExprStmt expr) = evalExpr env expr
+evalStmt env (BreakStmt tipo) = return Break
+evalStmt env (ReturnStmt Nothing) = return Nil
+evalStmt env (ReturnStmt (Just expr)) = evalExpr env exp
+
+--
+------------------------  Bloco IF ----------------------------------------------
+--
+
+-----------------BlockStmt (x:xs)--------------------------
+
+evalStmt env (BlockStmt (a:[])) = evalStmt env a
+evalStmt env (BlockStmt (a:as)) = 
+    case a of
+        (BreakStmt _) -> evalStmt env a
+        (ReturnStmt _) -> evalStmt env a
+        _ -> do evalStmt env a
+                evalStmt env (BlockStmt as)	
+			
+---------------------BLOCO IF------------------------------ 
+evalStmt env (IfSingleStmt expr ifBlock) = do
+    ret <- evalExpr env expr
+    case ret of 
+     --   err@(Error s) -> return err
+        Bool b -> if b == True then evalStmt env ifBlock else evalStmt env EmptyStmt
+
+--------------------BLOCO IF/ELSE------------------------
+evalStmt env (IfStmt expr ifBlock elseBlock) = do
+	ret <- evalExpr env expr
+	case ret of
+		(Bool b) -> if b then evalStmt env ifBlock else evalStmt env elseBlock
+	--	(Error _) -> return $ Error $  " not defined"
+		
+
+
+--
+---------------------------------------------------------------------------------------
+--
+
+--
+---------------------------------------BLOCO FOR---------------------------------------
+--
+evalStmt env(ForStmt inicio expressao incremento forBlock) = do 
+	evalInit env inicio
+	case expressao of
+		(Just a) -> do
+			val <- evalExpr env a
+			case val of
+				(Bool b) -> do
+					if b then do
+						eval <- evalStmt env forBlock
+						case eval of
+							Break -> return Nil
+							_ -> do
+								case incremento of
+									(Just expr) -> do evalExpr env expr	
+														evalStmt env (ForStmt NoInit expressao incremento forBlock)
+									(Nothing) -> evalStmt env (ForStmt NoInit expressao incremento forBlock)
+
+					
+					else evalStmt env EmptyStmt
+		(Nothing) -> do
+			eval <- evalStmt env forBlock
+			case eval of  
+                Break -> return Nil
+                _ -> do
+                    case incremento of
+						(Just expr) -> evalExpr env expr
+						(Nothing) -> return Nil
+                    evalStmt env (ForStmt NoInit expressao incremento comando)
+
+
+
+------------------------FORINIT----------------------------
+evalInit env (NoInit) = return Nil
+evalInit env (VarInit a) = (evalStmt env (VarDeclStmt a))
+evalInit env (ExprInit b) = (evalExpr env b)
+	
+--
+---------------------------------------------------------------------------------------
+--
+
+--
+-----------------------BLOCO WHILE--------------------------------------------------
+--
+evalStmt env(WhileStmt expresao whileBock) = do
+    val <- evalExpr env expressao                                               -- Avalia a expressão
+    case val of
+        (Bool b) -> if b then do                                                -- Se a expressão for booleana
+                        ehbreak <- evalStmt env whileBock                       -- Avalia se o comando é um break
+                        case ehbreak of
+                            Break -> return Nil                                 -- Se for, para a execução do programa
+                            _ -> evalStmt env (WhileStmt expressao whileBock)   -- Se for qualqer outra coisa, executa e chama o bloco novamente
+                    else return Nil
+     -- (Error _ -> return $ Error $  " not defined"           
+
+--
+---------------------------------------------------------------------------------------
+--
 
 -- Do not touch this one :)
 evaluate :: StateT -> [Statement] -> StateTransformer Value
--- Main.js
+++ Main.js
@@ -1,2 +1,31 @@
-var x = 0, y = 10;
+/*
+//Programa 1 - Testando if/else(Com e sem o break)
+var x = 5, y = 10, z;
 x + y;
+
+if( x > y) {
+	z = y;
+} else {
+	if(x == y) {
+		z = x;
+	} else {
+		break;
+		z = 50;
+	}
+	
+}
+*/
+
+
+//Programa 2 - Testando o For
+/*
+var x = 20;
+for (var i = 0; i < 50 ; i = i + 1){
+	x = 20+i;
+}
+*/
+var y = 10;
+
+for(var j = 1; j <5 ; j = j + 1){
+	y = 1 + j;
+}
